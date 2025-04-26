c03a945260072402a1db1c64c9980d0484bca9c2
ansan <anshanpool@gmail.com>
2025-04-26 17:37:03 +0545

    added the html parser

--- app/utils/scrapy/parser.py
+++ app/utils/scrapy/parser.py
@@ -1,36 +1,60 @@
 from bs4 import BeautifulSoup
 import re
 
-TEXT_TAG = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 
-            'span', 'li', 'div']
-VIDEO_TAG = ['iframe']
-IMG_TAG = ['img']
+def extract_metadata(metadata_block: str):
+    """
+    Extracts the metadata data and returns it, returns None if no metadata found
+    """
+    if metadata_block:
+        metadata_text = metadata_block.group(1)
+        matches = re.findall(r"(\w+):\s*(.+)", metadata_text)
+        
+        metadata = {key: value for key, value in matches}
+        return metadata
+    else:
+        return None
+
+def extract_text(text_block: str):
+    """
+    Extracts the texts data and returns it, returns None if empty string
+    """
+    if text_block:
+        cleaned_text = re.sub(r"---\n(.*?)\n---\n?", "", text_block, flags=re.DOTALL)
+        return cleaned_text
+    else:
+        return None
 
 def extract_from_html(response: str):
     """
-    Extract text, image and video content form the HTMl page.
+    Extracts the valuable informations from the string and
+    stores them in the structured manner.
+    
+    Args:
+        accepts 
     
     Returns:
-        dict: A dictionary containing lists of 'text', 
-            'video_links' and 'image_links'. Returns 
-            None if there's an issue fetching the page.
+        returns the dictionary containing main article and the
+        metadata regarding the the each article
     """
-    
     try:
-        html_content = BeautifulSoup(response.content, 'html.parser')
+        ### Extracting metadata from the web page ###
+        metadata_block = re.search(r"---\n(.*?)\n---", response, re.DOTALL)
+        content_metadata = extract_metadata(metadata_block=metadata_block)
         
-        text_content = []
-        for element in html_content.find_all(TEXT_TAG):
-            if element.name not in ['script', 'style']:
-                text = element.get_text(separator='n', strip=True)
-                if text:
-                    text_content.append(text)
-                    
-        return {
-            'text': list(set(text_content))
+        ### Extracting the articles from the web page ###
+        content_text = extract_text(text_block=response)
+        web_data = {
+            'metadata': content_metadata,
+            'texts': content_text
         }
-    except Exception as e:
-        return None
-    
-    
-    
+        return web_data
+        
+    except:
+        content_metadata = None
+        content_text = None
+        
+        web_data = {
+            'metadata': content_metadata,
+            'texts': content_text
+        }
+        return web_data
